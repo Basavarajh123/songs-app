4a8ee7d4f397a904b0245a252440798a914f4e48
Basavarajh123 <basavarajh556@gmail.com>
2024-03-12 19:12:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const express =require("express");
+const cors =require("cors");
+const {open}= require("sqlite");
+const sqlite3=require("sqlite3");
+
+const path= require('path');
+
+const dbPath = path.join(__dirname,"songs.db");
+
+const app= express();
+
+app.use(express.json());
+app.use(cors())
+
+let database=null;
+
+const initializeDbAndServer=async()=>{
+    try{
+        database= await open({
+            filename:dbPath,
+            driver:sqlite3.Database
+        })
+        app.listen(5002,async()=>{
+            console.log("Listening at Port http://localhost:5002")
+        })
+
+    }catch(error){
+        console.log(`DB Error : ${error.message}`)
+    }
+   
+}
+
+initializeDbAndServer()
+
+app.get("/",(request,response)=>{
+    response.send("Welcome to Songs World")
+})
+
+app.get("/songs",async(request,response)=>{
+    const sqlQuery= `SELECT * FROM songs`
+
+    const data = await database.all(sqlQuery)
+    response.send(data)
+})
---
+++ mysql.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE songs (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--                     songUrl VARCHAR(200) NOT NULL ,
+--                     tumbnalImageUrl VARCHAR(200) NOT NULL 
+
+
+-- )
+
+-- INSERT INTO songs(songUrl,tumbnalImageUrl)
+--             VALUES("https://res.cloudinary.com/dvvldj42g/video/upload/v1708696245/Artist_-_line_in_track_04_eyshjs.mp3","https://res.cloudinary.com/dvvldj42g/image/upload/v1708329093/company_logo_QFt1gWPZ_bkxpno.png")
+
+-- INSERT INTO songs(songUrl,tumbnalImageUrl)
+--             VALUES("https://res.cloudinary.com/dvvldj42g/video/upload/v1708696235/B.HUTTIDDU_HOLIMANI-SHIMOGA_SUBBANNA_cvrq6z.mp3","https://res.cloudinary.com/dvvldj42g/image/upload/v1708329093/company_logo_QFt1gWPZ_bkxpno.png");
+
+-- SELECT * FROM songs
